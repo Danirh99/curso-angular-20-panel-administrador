2a7dec0a88415ff11d14ecd9062b5bee37ffb488
Danirh99 <danicreations698@gmail.com>
2025-09-23 19:51:54 +0200

    Terminada Clase 1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # curso-angular-20-panel-administrador
+
+Ejecutar
+
+```bash
+docker compose up
+```
+
+Abrir un navegador e ir a `http://localhost:4200/`
--- mi-app/src/app/app.ts
+++ mi-app/src/app/app.ts
@@ -1,11 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {MainLayout} from './shared/layout/main-layout/main-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MainLayout],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('mi-app');
