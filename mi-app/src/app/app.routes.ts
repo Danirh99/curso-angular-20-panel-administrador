0eb9c10d5acf16d25b8b17d198e366a4ad28f209
Danirh99 <danicreations698@gmail.com>
2025-10-01 17:56:20 +0200

    Terminada Clase 2

--- mi-app/src/app/app.routes.ts
+++ mi-app/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import {Routes} from '@angular/router';
+import {Dashboard} from './pages/dashboard/dashboard';
+import {Login} from './pages/login/login';
+import {Usuarios} from './pages/usuarios/usuarios';
+import {Clientes} from './pages/clientes/clientes';
+import {NotFound} from './pages/errors/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Dashboard },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'login', component: Login },
+  { path: 'usuarios', component: Usuarios },
+  { path: 'clientes', component: Clientes },
+  { path: '**', component: NotFound },
+];
+
+export const navRoutes = [
+  { path: '/dashboard', label: 'Dashboard'},
+  { path: '/login', label: 'Login'},
+  { path: '/usuarios', label: 'Usuarios'},
+  { path: '/clientes', label: 'Clientes'},
+]
